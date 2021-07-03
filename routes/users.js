bf27ca4db505a038dffa0fd1d25c5b1b0dae3f6c
Viraj24Gupta <viraj24Gupta@gmailcom>
2021-07-03 11:10:21 +0530

    /./.

--- routes/users.js
+++ routes/users.js
@@ -2,47 +2,26 @@ var express = require('express');
 var router = express.Router();
 let admin = require('firebase-admin');
 let bodyParser = require('body-parser');
+require("dotenv/config");
 const path = require('path');
-
-router.use(express.static(path.join(__dirname,'./')));
-
 const serviceAccount = require('../key.json');
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
 });
-
 const db = admin.firestore();
+const storage = admin.storage();
 router.use(bodyParser.urlencoded({extended: false}));
-
+router.use(express.static(path.join(__dirname,'./')));
 
 router.get('/data', async(req, res)=> {
     console.log("fetching data");
     const all = await db.collection('users').orderBy("timestamp", "desc");
-    // const observer = doc.onSnapshot(docSnapshot => {
-    //     console.log(`Received doc snapshot: ${docSnapshot}`);
-    //     console.log("reached x");
-    //     for(i in docSnapshot){
-    //         res.json(docSnapshot[i]);
-    //     }
-    //     }, err => {
-    //     console.log(`Encountered error: ${err}`);
-    // });
     const snapshot1 = await all.get().then((querySnapshot) => {
         var docs = querySnapshot.docs.map(doc => doc.data());
         res.json(docs);
     });
-
-    // let response=[];
-    // snapshot.forEach(doc => {
-    //     response.push(doc.data());
-    //     // console.log(doc.id, '=>', doc.data());
-    // });
-    // res.json(response);
-
-
 });
-
 
 router.get('/id',async(req,res)=>{
     const all = await db.collection('users').orderBy("timestamp", "desc");
@@ -54,11 +33,17 @@ router.get('/id',async(req,res)=>{
 
 router.post('/change',async(req,res)=>{
     var ad_num= req.body.ad_num;
+    var ad_url= req.body.ad_url;
     var clicked_id= req.body.changed;
     console.log(ad_num);
+    console.log(ad_url);
     console.log(clicked_id);
     db.collection('users').doc(clicked_id).update({ aadharNumber: ad_num });
     db.collection('users').doc(clicked_id).update({ Verify: "Yes" });
+    if(typeof ad_url != "undefined"){
+        await storage.bucket("krib2downgrade-1.appspot.com").file("AADHAR CARD/"+clicked_id+".jpg").delete();
+        db.collection('users').doc(clicked_id).update({ aadharUrl: null });
+    }
     res.redirect("/home");
 });
 
@@ -76,30 +61,5 @@ router.post("/", async(req,res)=>{
     });
     res.render('err',{msg: 'WRONG USERNAME OR PASSWORD',path:'/signin'});
 });
-
-
-// router.get('/storage', async(req,res)=>{
-//     const storageBucket = admin.storage().bucket( 'gs://krib2downgrade-1.appspot.com' );
-//     const getRemoteImages = async() => {
-//         const imagePromises = posts.map( (item, index) => admin
-//             .firestore().collection('AADHAR CARD').get().then(querySnapshot => {
-//                 // querySnapshot is an array but I only want the first instance in this case
-//                 const docRef = querySnapshot.docs[0];
-//                 // the property "url" was what I called the property that holds the name of the file in the "posts" database
-//                 const fileName = docRef.data().url;
-//                 return storageBucket.file( fileName ).getSignedUrl({
-//                     action: "read",
-//                     expires: '03-17-2025' // this is an arbitrary date
-//                 })
-//             })
-//             // chained promise because "getSignedUrl()" returns a promise
-//             .then((data) => data[0])
-//             .catch(err => console.log('Error getting document', err))
-//         )
-//         // returns an array of remote image paths
-//         const imagePaths = await Promise.all(imagePromises);
-//         return imagePaths
-//     }
-// });
 
 module.exports = router;
